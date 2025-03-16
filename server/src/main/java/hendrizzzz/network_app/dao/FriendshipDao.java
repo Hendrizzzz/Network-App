11d011affaa11c7bde9b6167b80d793ce849bc3f
Hendrizzzz <jimhendrixbageo@gmail.com>
2025-03-16 18:32:34 +0800

    Finish the dao for the Friendship

--- server/src/main/java/hendrizzzz/network_app/dao/FriendshipDao.java
+++ server/src/main/java/hendrizzzz/network_app/dao/FriendshipDao.java
@@ -2,19 +2,49 @@ package hendrizzzz.network_app.dao;
 
 import hendrizzzz.network_app.model.Friendship;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class FriendshipDao {
 
     public void addFriendship(Friendship friendship) {
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement(Sql.addFriendship)) {
 
+            statement.setInt(1, friendship.getUserId());
+            statement.setInt(2, friendship.getOther_userId());
+            statement.setString(3, friendship.getStatus());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
     public void updateFriendship(Friendship friendship, String newStatus) {
+        try (Connection connection = DatabaseConnection.getConnection();
+            PreparedStatement statement = connection.prepareStatement(Sql.updateFriendship)) {
 
+            statement.setString(1, newStatus);
+            statement.setInt(2, friendship.getUserId());
+            statement.setInt(3, friendship.getOther_userId());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
-    public void removeFriendship(Friendship friendship) {
+    public int removeFriendship(int userId, int otherUserId) {
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement(Sql.removeFriendship)) {
 
+            statement.setInt(1, userId);
+            statement.setInt(2, otherUserId);
+            return statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
