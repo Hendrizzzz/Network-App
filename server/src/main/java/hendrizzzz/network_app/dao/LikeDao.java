108765d74f9f154733cd8884fe212a2fffec6878
Hendrizzzz <jimhendrixbageo@gmail.com>
2025-03-16 18:32:54 +0800

    Finish the dao for the like

--- server/src/main/java/hendrizzzz/network_app/dao/LikeDao.java
+++ server/src/main/java/hendrizzzz/network_app/dao/LikeDao.java
@@ -2,15 +2,35 @@ package hendrizzzz.network_app.dao;
 
 import hendrizzzz.network_app.model.Like;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class LikeDao {
 
-    public void addLike(Like like) {
+    public void addLike(Like like, String type, int id) {
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement(Sql.addLike(type))) {
 
+            statement.setBoolean(1, like.isAPost());
+            statement.setInt(2, like.getUser_id_who_liked());
+            statement.setInt(3, id);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
     public void removeLike(int id) {
+        try (Connection connection = DatabaseConnection.getConnection();
+            PreparedStatement statement = connection.prepareStatement(Sql.removeLike)) {
 
+            statement.setInt(1, id);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
