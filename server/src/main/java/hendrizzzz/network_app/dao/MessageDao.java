d1651b3935e43b2ddd96dc56e3a4dc2f46891d9d
Hendrizzzz <jimhendrixbageo@gmail.com>
2025-03-16 18:33:16 +0800

    Finish the dao for the message

--- server/src/main/java/hendrizzzz/network_app/dao/MessageDao.java
+++ server/src/main/java/hendrizzzz/network_app/dao/MessageDao.java
@@ -2,20 +2,50 @@ package hendrizzzz.network_app.dao;
 
 import hendrizzzz.network_app.model.Message;
 
+import javax.xml.crypto.Data;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class MessageDao {
 
 
     public void addMessage(Message message) {
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement(Sql.addMessage)) {
 
+            statement.setInt(1, message.getSenderId());
+            statement.setInt(2, message.getReceiverId());
+            statement.setString(3, message.getContents());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
     public void deleteMessage(int id) {
+        try (Connection connection = DatabaseConnection.getConnection();
+            PreparedStatement statement = connection.prepareStatement(Sql.deleteMessage)) {
 
+            statement.setInt(1, id);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+
+        }
     }
 
 
     public void editMessage(int id, String newMessage) {
+        try (Connection connection = DatabaseConnection.getConnection();
+            PreparedStatement statement = connection.prepareStatement(Sql.editMessage)) {
+
+            statement.setString(1, newMessage);
+            statement.setInt(2, id);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
