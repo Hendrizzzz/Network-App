b21afc80049634ca2012b5ecb0e79ae18a49c124
Hendrizzzz <jimhendrixbageo@gmail.com>
2025-03-16 18:31:23 +0800

    Add new sql commands

--- server/src/main/java/hendrizzzz/network_app/dao/Sql.java
+++ server/src/main/java/hendrizzzz/network_app/dao/Sql.java
@@ -26,11 +26,45 @@ public class Sql {
 
     // PostDAO
     static String addPost = "INSERT into post (author_id, contents, privacy) VALUES (?, ?, ?)";
-
     static String deletePost = "DELETE FROM post WHERE id = ?";
     static String updatePost = "UPDATE post SET contents = ?, privacy = ? WHERE id = ?";
     static String incrementCommentCount = "UPDATE post SET comment_count = comment_count + 1 WHERE id = ?";
+    static String decrementCommentCount = "UPDATE post SET comment_count = comment_count - 1 WHERE id = ?";
     static String getCommentCount = "SELECT comment_count FROM post WHERE post_id = ?";
+
+
+    // NotificationDAO
+    static String markRead = "UPDATE notifications SET is_read = true WHERE id = ?";
+    static String addNotification(String type) {return String.format("INSERT INTO notifications (user_id, notification_type, content, %s) VALUES (?, ?, ?, ?)", type);}
+    static String deleteNotification = "DELETE FROM notifications WHERE id = ?";
+
+
+    // MessageDAO
+    static String addMessage = "INSERT INTO message (sender_id, receiver_id, contents) VALUES (?, ?, ?)";
+    static String deleteMessage = "DELETE from message WHERE id = ?";
+    static String editMessage = "UPDATE message SET contents = ? WHERE id = ?";
+
+
+    // LikeDAO
+    static String addLike(String type) { return "INSERT INTO like (is_a_post, user_who_liked, %s) VALUES (?, ?, ?)";}
+    static String removeLike = "DELETE FROM like WHERE id = ?";
+
+
+    // FriendshipDAO
+    static String addFriendship = "INSERT INTO friends (user_id, other_user_id, status) VALUES (?, ?, ?)";
+    static String updateFriendship = "UPDATE friends SET status = ? WHERE user_id = ? AND other_user_id = ?";
+    static String removeFriendship = "DELETE from friends WHERE user_id = ? AND other_user_id = ?";
+
+
+    // FollowersDAO
+    static String addFollowers = "INSERT INTO followers (user_id, follower_id) VALUES (?, ?)";
+    static String removeFollower = "DELETE FROM followers WHERE user_id = ? AND follower_id = ?";
+
+
+    // CommentDAO
+    static String addComment = "INSERT INTO comment (post_id, author_id, content) VALUES (?, ?, ?)";
+    static String deleteComment = "DELETE FROM comment WHERE id = ?";
+    static String editComment = "UPDATE comment SET content = ? WHERE id = ?";
 
 
 }
