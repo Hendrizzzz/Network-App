c27ecb8365b500e173be01c95bfb313850265eaf
Hendrizzzz <jimhendrixbageo@gmail.com>
2025-03-16 13:22:38 +0800

    Add a database connector

--- server/src/main/java/hendrizzzz/network_app/dao/DatabaseConnection.java
+++ server/src/main/java/hendrizzzz/network_app/dao/DatabaseConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/network%20app";
-    private static final String USER = "Hendrixxx";
+    private static final String USER = "root";
     private static final String PASSWORD = "jimhendrix24";
 
     public static Connection getConnection() throws SQLException {
