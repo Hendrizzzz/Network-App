b949a077b05b988c9ae9ced88cc30930c7609f69
Hendrizzzz <jimhendrixbageo@gmail.com>
2025-03-16 18:32:10 +0800

    Finish the dao for the Followers

--- server/src/main/java/hendrizzzz/network_app/dao/FollowersDao.java
+++ server/src/main/java/hendrizzzz/network_app/dao/FollowersDao.java
@@ -2,15 +2,35 @@ package hendrizzzz.network_app.dao;
 
 import hendrizzzz.network_app.model.Followers;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class FollowersDao {
 
 
     public void addFollowers(Followers followers) {
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement(Sql.addFollowers)) {
 
+            statement.setInt(1, followers.getUserId());
+            statement.setInt(2, followers.getFollowerId());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
     public void removeFollowers(Followers followers) {
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement(Sql.removeFollower)) {
 
+            statement.setInt(1, followers.getUserId());
+            statement.setInt(2, followers.getFollowerId());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
