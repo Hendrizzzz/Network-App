fc6147cea9b53a6a79e8755671b0a558aecbbd17
Hendrizzzz <jimhendrixbageo@gmail.com>
2025-03-16 18:33:34 +0800

    Finish the dao for the Notifications

--- server/src/main/java/hendrizzzz/network_app/dao/NotificationDao.java
+++ server/src/main/java/hendrizzzz/network_app/dao/NotificationDao.java
@@ -2,18 +2,49 @@ package hendrizzzz.network_app.dao;
 
 import hendrizzzz.network_app.model.Notification;
 
+import javax.xml.crypto.Data;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class NotificationDao {
 
     public void markRead(int id) {
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement(Sql.markRead)) {
 
+            statement.setInt(1, id);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public void addNotification(Notification notification) {
+    public void addNotification(Notification notification, String column, int id) {
+        try (Connection connection = DatabaseConnection.getConnection();
+            PreparedStatement statement = connection.prepareStatement(Sql.addNotification(column))) {
 
+            statement.setInt(1, notification.getUserId());
+            statement.setString(2, notification.getNotificationType());
+            statement.setString(3, notification.getContents());
+            statement.setInt(4, id);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
     public void deleteNotification(int id) {
+        try (Connection connection = DatabaseConnection.getConnection();
+            PreparedStatement statement = connection.prepareStatement(Sql.deleteNotification)) {
 
+            statement.setInt(1, id);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
+
+
 }
