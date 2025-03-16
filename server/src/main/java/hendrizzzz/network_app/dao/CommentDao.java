636a79d2f52140a48347279116bd39d968dacc2e
Hendrizzzz <jimhendrixbageo@gmail.com>
2025-03-16 18:31:54 +0800

    Finish the dao for the Comment

--- server/src/main/java/hendrizzzz/network_app/dao/CommentDao.java
+++ server/src/main/java/hendrizzzz/network_app/dao/CommentDao.java
@@ -2,19 +2,47 @@ package hendrizzzz.network_app.dao;
 
 import hendrizzzz.network_app.model.Comment;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class CommentDao {
 
     public void addComment(Comment comment) {
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement(Sql.addComment)) {
 
+            statement.setInt(1, comment.getPostId());
+            statement.setInt(2, comment.getAuthorId());
+            statement.setString(3, comment.getContent());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
     public void deleteComment(int id) {
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement(Sql.deleteComment)) {
 
+            statement.setInt(1, id);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
     public void editComment(int id, String newContents) {
+        try (Connection connection = DatabaseConnection.getConnection();
+            PreparedStatement statement = connection.prepareStatement(Sql.editComment)) {
 
+            statement.setString(1, newContents);
+            statement.setInt(2, id);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
